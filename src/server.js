d84ce2dcb6fc0d31c19672fbd5b301571d29c6ee
Ally Rippley <ally.rippley@gmail.com>
2024-04-18 21:07:20 -0500

    update console.log

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log('MongoDB connected');
+  console.log('MongoDB connected2');
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
   .catch(err => console.log(err));
